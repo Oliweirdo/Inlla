b96ca0fd060dac6d9f11559cf4aff5ce1ac27e1d
Oliwia <Oliweirdo@gmail.com>
2022-07-17 09:55:45 +0200

    add password for subpage

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,21 @@ document.querySelectorAll(".nav-link").forEach(n => n.
         hamburger.classList.remove("active");
         navMenu.classList.remove("active");
     }))
+
+    function run() {
+        var testV = 1;
+        var pass1 = prompt('Strona dostępna jest wyłącznie dla klientów. Proszę podaj hasło by przejść dalej.',' ');
+        while (testV < 2) {
+            if (pass1.toLowerCase() == "tost") {
+                alert('Hasło prawidłowe, zapraszam!');
+                window.open('prepare.html');
+                break;
+            } 
+            testV+=1;
+            var pass1 = prompt('Odmowa dostępu, hasło jest nieprawidłowe. Wpisz hasło ponownie.','Password');
+        }
+        if (pass1.toLowerCase()!="password" & testV ==3) history.go(-1);
+        return " ";
+    }
+    
+    
